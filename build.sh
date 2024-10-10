ce72b348890656d969b5f95dc9f69f1596f77af8
Fred Juhlin <fred.juhlin@gmail.com>
2024-10-10 16:09:26 +0200

    2.0.0

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-docker build --build-arg CHIP=artpec8 . -t detectx
+docker build --build-arg CHIP=artpec7 . -t detectx
 docker cp $(docker create detectx):/opt/app ./build
 cp ./build/*.eap .
 rm -rf ./build
