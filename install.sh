b92837fce104577b828a5b72f16e07a7733665da
Joakim Roubert <joakimr@axis.com>
2026-02-11 11:50:48 +0100

    Shellscript housekeeping
    - Use /bin/sh instead of bash for better portability
    - Add means to easily run build.sh with podman instead of docker
    - Use single quotes for static strings
    - Output error messages to stderr
    - Use single echo call instead of multiple ones in a row
    - Use `find` instead of `ls` in some places for better stability
    - Add -eu to scripts to fail early and avoid undefined behavior
    - Use tabs for indent
    
    Change-Id: If7651248cb8a9199926844fbc25313788a8f007b
    Signed-off-by: Joakim Roubert <joakimr@axis.com>

--- install.sh
+++ install.sh
@@ -1,26 +1,25 @@
-#!/bin/bash
+#!/bin/sh -eu
 #
 # Install DetectX EAP to Axis camera
-#
 
-CAMERA_HOST="${1:-front.internal}"
-USERNAME="nodered"
-PASSWORD="rednode"
+CAMERA_HOST=${1:-front.internal}
+USERNAME=nodered
+PASSWORD=rednode
 
 # Find the first .eap file in the current directory
-EAP_FILE=$(ls -1 *.eap 2>/dev/null | head -n 1)
+EAP_FILE=$(find . -maxdepth 1 -name \*.eap 2>/dev/null | sort | head -n 1)
 
 if [ -z "$EAP_FILE" ]; then
-    echo "ERROR: No .eap file found in the current directory"
-    exit 1
+	echo 'ERROR: No .eap file found in the current directory' >&2
+	exit 1
 fi
 
 echo "Installing $EAP_FILE to $CAMERA_HOST..."
 
 # Upload using curl with digest authentication
 curl --digest -u "$USERNAME:$PASSWORD" \
-    -F "packfil=@$EAP_FILE;type=application/octet-stream" \
-    "http://$CAMERA_HOST/axis-cgi/applications/upload.cgi"
+	-F "packfil=@$EAP_FILE;type=application/octet-stream" \
+	"http://$CAMERA_HOST/axis-cgi/applications/upload.cgi"
 
-echo ""
-echo "Done."
+echo '
+Done.'
